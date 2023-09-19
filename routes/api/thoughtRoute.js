cb468fc716637c3d12b4e0ac6e72ef15987148e3
Hasan Malik <hmalik0412@gmail.com>
2023-09-19 06:41:50 +0100

    comments for routes added

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,31 +1,36 @@
+// Import the Express.js router
 const router = require("express").Router();
+
+// Import the thoughtController functions
 const {
-    get_all_thoughts,
-    get_thought_by_id,
-    add_thought,
-    update_thought,
-    remove_thought,
-    add_reaction,
-    remove_reaction
+  get_all_thoughts,
+  get_thought_by_id,
+  add_thought,
+  update_thought,
+  remove_thought,
+  add_reaction,
+  remove_reaction
 } = require("../../controllers/thoughtController");
 
+// Define routes for handling thought-related operations
 router
-    .route("/")
-    .get(get_all_thoughts)
-    .post(add_thought);
+  .route("/")
+  .get(get_all_thoughts) // Get all thoughts
+  .post(add_thought); // Add a new thought
 
 router
-    .route("/:id")
-    .get(get_thought_by_id)
-    .put(update_thought)
-    .delete(remove_thought);
+  .route("/:id")
+  .get(get_thought_by_id) // Get a thought by its ID
+  .put(update_thought) // Update a thought by its ID
+  .delete(remove_thought); // Remove a thought by its ID
 
 router
-    .route("/:thoughtId/reactions")
-    .post(add_reaction);
+  .route("/:thoughtId/reactions")
+  .post(add_reaction); // Add a reaction to a thought
 
 router
-    .route("/:thoughtId/reactions/:reactionId")
-    .delete(remove_reaction)
+  .route("/:thoughtId/reactions/:reactionId")
+  .delete(remove_reaction); // Remove a reaction from a thought
 
+// Export the router for use in your application
 module.exports = router;
--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,31 +1,36 @@
+// Import the Express.js router
 const router = require("express").Router();
+
+// Import the userController functions
 const {
-    get_all_users,
-    get_user_by_id,
-    add_user,
-    update_user,
-    remove_user,
-    add_friend,
-    remove_friend
+  get_all_users,
+  get_user_by_id,
+  add_user,
+  update_user,
+  remove_user,
+  add_friend,
+  remove_friend
 } = require("../../controllers/userController");
 
+// Define routes for handling user-related operations
 router
-    .route("/")
-    .get(get_all_users)
-    .post(add_user);
+  .route("/")
+  .get(get_all_users) // Get all users
+  .post(add_user); // Add a new user
 
 router
-    .route("/:id")
-    .get(get_user_by_id)
-    .put(update_user)
-    .delete(remove_user);
+  .route("/:id")
+  .get(get_user_by_id) // Get a user by their ID
+  .put(update_user) // Update a user by their ID
+  .delete(remove_user); // Remove a user by their ID
 
 router
-    .route("/:id/friends/:friendId").
-    post(add_friend);
+  .route("/:id/friends/:friendId")
+  .post(add_friend); // Add a friend to a user's friend list
 
 router
-    .route("/:id/friends/:friendId")
-    .delete(remove_friend);
+  .route("/:id/friends/:friendId")
+  .delete(remove_friend); // Remove a friend from a user's friend list
 
+// Export the router for use in your application
 module.exports = router;
